3e776126086d7f0d4ed942be5c3676f216004fa5
Marilena Tz. <100719154+marilenatz99@users.noreply.github.com>
2024-04-03 15:15:53 +0000

    01_14_e

---
+++ front-end/src/components/ShoppingCartList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-if="cartItems.length > 0">
+    <div class="product-container" v-for="product in cartItems" :key="product.id">
+      <img class="product-image" :src="product.imageName" />
+      <div class="details-wrap">
+        <h3>{{ product.name }}</h3>
+        <p>{{ product.price }}</p>
+      </div>
+      <button class="remove-button">Remove from cart</button>
+    </div>
+    <button class="checkout-button">Proceed to Checkout</button>
+  </div>
+  <div v-if="cartItems.length === 0">You currently have no items in yout cart.</div>
+</template>
+
+<script>
+export default {
+  name: "ShoppingCartList",
+  props: ['cartItems']
+}
+</script>
--- front-end/src/pages/ShoppingCartPage.vue
+++ front-end/src/pages/ShoppingCartPage.vue
@@ -1,25 +1,17 @@
 <template>
   <h1>Shopping Cart</h1>
-  <div v-if="cartItems.length > 0">
-    <div class="product-container" v-for="product in cartItems" :key="product.id">
-      <img class="product-image" :src="product.imageName" />
-      <div class="details-wrap">
-        <h3>{{ product.name }}</h3>
-        <p>{{ product.price }}</p>
-      </div>
-      <button class="remove-button">Remove from cart</button>
-    </div>
-    <button class="checkout-button">Proceed to Checkout</button>
-  </div>
-  <div v-if="cartItems.length === 0">You currently have no items in yout cart.</div>
-  
+  <ShoppingCartList :cartItems="cartItems" />
 </template>
 
 <script>
+import ShoppingCartList from '../components/ShoppingCartList.vue';
 import { cartItems } from '@/temp-data';
 
 export default{
   name: "ShoppingCartPage",
+  components: {
+    ShoppingCartList
+  },
   data(){
     return{
       cartItems
