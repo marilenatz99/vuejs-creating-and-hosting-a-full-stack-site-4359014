ff8d39cc4a6aa19f1027112d6f3c93d53ca09eef
Marilena Tz. <100719154+marilenatz99@users.noreply.github.com>
2024-04-03 14:48:45 +0000

    01_10_e

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,15 +1,17 @@
 <!-- eslint-disable vue/valid-template-root -->
 <template>
-  <h1>Two Trees Olive Oil</h1>
+  <NavBar />
   <div class="page-wrap">
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-
+import NavBar from '@/components/NavBar.vue';
 export default {
   name: 'App',
-  components: {}
+  components: {
+    NavBar,
+  }
 }
 </script>
