ee297c6a56b1d2a6fbeeb2575ce09792da382389
Marilena Tz. <100719154+marilenatz99@users.noreply.github.com>
2024-04-04 15:11:04 +0000

    03_02_e

--- back-end/src/temp-data.js
+++ back-end/src/temp-data.js
@@ -1,52 +1,43 @@
-// import basilBottle from './assets/basil-bottle.webp';
-// import lemonBottle from './assets/lemon-bottle.webp';
-// import garlicBottle from './assets/garlic-bottle.webp';
-// import rosemaryBottle from './assets/rosemary-bottle.webp';
-// import chiliBottle from './assets/chili-bottle.webp';
-// import delicateBottle from './assets/delicate-bottle.webp';
-// import mediumBottle from './assets/medium-bottle.webp';
-// import boldBottle from './assets/bold-bottle.webp';
-
 export const cartItems = ['123', '345'];
 
 export const products = [{
     id: '123',
     name: 'Basil',
     price: '$26.00',
-    // imageName: basilBottle,
+    imageUrl: './assets/basil-bottle.webp',
 }, {
     id: '234',
     name: 'Lemon',
     price: '$26.00',
-    // imageName: lemonBottle,
+    imageUrl: './assets/lemon-bottle.webp',
 }, {
     id: '345',
     name: 'Garlic',
     price: '$26.00',
-    // imageName: garlicBottle,
+    imageUrl: './assets/garlic-bottle.webp',
 }, {
     id: '456',
     name: 'Rosemary',
     price: '$26.00',
-    // imageName: rosemaryBottle,
+    imageUrl: './assets/rosemary-bottle.webp',
 }, {
     id: '567',
     name: 'Chili',
     price: '$26.00',
-    // imageName: chiliBottle,
+    imageUrl: './assets/chili-bottle.webp',
 }, {
     id: '678',
     name: 'Delicate',
     price: '$24.50',
-    // imageName: delicateBottle,
+    imageUrl: './assets/delicate-bottle.webp',
 }, {
     id: '789',
     name: 'Medium',
     price: '$24.50',
-    // imageName: mediumBottle,
+    imageUrl: './assets/medium-bottle.webp',
 }, {
     id: '890',
     name: 'Bold',
     price: '$24.50',
-    // imageName: boldBottle,
+    imageUrl: './assets/bold-bottle.webp',
 }];
