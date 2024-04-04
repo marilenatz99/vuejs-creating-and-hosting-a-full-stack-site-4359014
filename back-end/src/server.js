ba485adac2ecc55285d94ad27d6895e1155af241
Marilena Tz. <100719154+marilenatz99@users.noreply.github.com>
2024-04-04 13:58:58 +0000

    02_08_e

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -17,8 +17,13 @@ app.get('/products', (req, res) => {
   res.json(products);
 });
 
+function populateCartIds(ids) {
+  return ids.map(id => products.find(product => product.id === id))
+}
+
 app.get('/cart', (req, res) => {
-  res.json(cartItems);
+  const populatedCart = populateCartIds(cartItems);
+  res.json(populatedCart);
 });
 
 app.get('/products/:productId', (req, res) => {
@@ -30,77 +35,18 @@ app.get('/products/:productId', (req, res) => {
   res.json(product);
 });
 
-// app.post('/addToCart', (req, res) => {
-//   // Find the item in our list of items by its id
-//   const itemInList = products.find(item => item.id == req.body.id);
-
-//   // If we didn't  find an item with that id then send back a 400 error
-//   if (!itemInList) {
-//     return res.status(400).json({ message: "Item not found!" });
-//   }
-
-//   let quantity = req.body.quantity || 1;
-
-//   // Add the new item to the  end of our array for now
-//   // We will talk about doing this more efficiently in class
-//   cartItems.push({ ...itemInList, quantity: quantity });
-
-//   res.status(201).json(cartItems[cartItems.length - 1]);
-// });
-
-// // Make sure you understand what is happening here before moving on
-// app.delete("/removeFromCart", (req, res) => {
-//   const index = cartItems.findIndex(i => i.id === req.body.id);
-
-//   if (index === -1) {
-//     return res.status(400).json({ message: 'ID not found' })
-//   }
-
-//   // Remove the item from the array
-//   cartItems.splice(index, 1);
-
-//   res.status(200).json(cartItems);
-// })
-
-// function calculateTotal(items) {
-//   return items.reduce((total, item) => total + (item.price * item.quantity), 0)
-// }
-
-// app.post( "/checkout", (req, res) =>  {
-//   const showSummary = () => res.status(200).json(cartItems.map(i => ({...i, price: i.price * i.quantity})) );
-
-//   // If there are no items in the user's shopping cart then display a summary of their empty shopping cart
-//   if (cartItems.length ===  0) return showSummary();
-
-//   const donePaying = () => {
-//     // Reset the users shopping cart
-//     cartItems.length=0;
-
-//     // Display a payment processing page or a receipt of some sort perhaps?
-//     res.status(200).send('Thank You For Your Purchase!');
-//   };
-
-//   // Calculate how much the customer owes and pass that amount along with the function to display the purchase summary when ready to pay
-
-// });
-
-// app.get('/cart', (req, res) => {
-//   res.json({ items: cartItems, total: calculateTotal(cartItems) });
-// });
-
-
-
 app.post('/cart', (req, res) => {
   const productId = req.body.id;
-  const product = products.find(product => product.id === productId);
-  cartItems.push(product);
-  res.json(cartItems);
+  cartItems.push(productId);
+  const populatedCart = populateCartIds(cartItems);
+  res.json(populatedCart);
 })
 
 app.delete('/cart/:productId', (req, res) => {
   const productId = req.params.productId;
-  cartItems = cartItems.filter(product => product.id !== productId);
-  res.json(cartItems);
+  cartItems = cartItems.filter(id => id !== productId);
+  const populatedCart = populateCartIds(cartItems);
+  res.json(populatedCart);
 })
 
 
