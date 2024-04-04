cb163108911ba0dba41086d90bb503e2387048af
Marilena Tz. <100719154+marilenatz99@users.noreply.github.com>
2024-04-04 12:39:46 +0000

    02_04_e

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,10 +1,28 @@
-import express, { response } from 'express';
+import express from 'express';
+import { cartItems, products } from './temp-data';
 
 const app = express();
 
-app.get('/hello', (req, res) =>{
+app.get('/hello', (req, res) => {
   res.send('Hello!');
 })
+
+app.get('/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/cart', (req, res) => {
+  res.json(cartItems);
+});
+
+app.get('/products/:productId', (req, res) => {
+  const productId = req.params.productId;
+  const product = products.find(product => product.id === productId);
+  if (!product) {
+    return res.status(404).json({ message: "Product not found" });
+  }
+  res.json(product);
+});
 
 app.listen(8000, () => {
   console.log(new Date(), "Server is running on port  8000");
